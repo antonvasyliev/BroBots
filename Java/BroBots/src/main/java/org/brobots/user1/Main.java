55e3a1a87aaebde198323240930826a468867885
anton <anton.v.vasyliev@gmail.com>
2016-10-23 12:35:40 +0300

    test

--- Java/BroBots/src/main/java/org/brobots/user1/Main.java
+++ Java/BroBots/src/main/java/org/brobots/user1/Main.java
@@ -4,4 +4,9 @@ package org.brobots.user1;
  * Created by anton on 23.10.2016.
  */
 public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("ss");
+    }
+
 }
---
+++ Java/BroBots/src/main/test/java/TempTest.java
@@ -0,0 +1,5 @@
+/**
+ * Created by anton on 23.10.2016.
+ */
+public class TempTest {
+}
